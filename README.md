9384ace7141c41853024432a7110fdeb779c570f
Yuliya Chernobrovkina <yuliya.chernobrovkina@softcom.su>
2023-10-22 12:20:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# gdb server
+## Usage
+Follow this to compile project
+```
+mkdir build
+cd build
+cmake ..
+make
+```
+Run it with
+```
+./main
+
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main()
+{
+    printf("run something\n");
+    return 0;
+}
